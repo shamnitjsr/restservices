4a27bd46b91a3397568a5ef66c32b9dec1ea668e
shamnitjsr <46891976+shamnitjsr@users.noreply.github.com>
2022-06-23 01:55:27 +0530

    humana_Skelton

---
+++ humanacarefirstmanagementapi/src/main/java/com/nitjsr/HumanacarefirstmanagementapiApplication.java
@@ -0,0 +1,13 @@
+package com.nitjsr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumanacarefirstmanagementapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HumanacarefirstmanagementapiApplication.class, args);
+	}
+
+}
