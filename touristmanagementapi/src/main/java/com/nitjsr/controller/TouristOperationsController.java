248a8a6e02a70d24b2d1c4c9883cec9f14c0eecf
shamnitjsr <46891976+shamnitjsr@users.noreply.github.com>
2022-06-27 00:21:03 +0530

    Curd_Operation_done

--- touristmanagementapi/src/main/java/com/nitjsr/controller/TouristOperationsController.java
+++ touristmanagementapi/src/main/java/com/nitjsr/controller/TouristOperationsController.java
@@ -1,9 +1,16 @@
 package com.nitjsr.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,4 +38,65 @@ public class TouristOperationsController {
 		}
 
 	}
+
+	@GetMapping("/findAll")
+	public ResponseEntity<?> displayTourist() {
+		try {
+			List<Tourist> list = service.fetchAllTourists();
+			return new ResponseEntity<List<Tourist>>(list, HttpStatus.OK);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new ResponseEntity<String>("Problem in fetching Tourist", HttpStatus.INTERNAL_SERVER_ERROR);// 500
+																												// server
+		}
+	}
+
+	@GetMapping("/find/{id}")
+	public ResponseEntity<?> displayTouristById(@PathVariable("id") Integer id) {
+		try {
+			Tourist tourist = service.fetchTouristById(id);
+			return new ResponseEntity<Tourist>(tourist, HttpStatus.OK);
+		} catch (Exception e) {
+
+			return new ResponseEntity<String>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);// 500
+		}
+	}
+
+	@PutMapping("/modify")
+	public ResponseEntity<String> modifyTourist(@RequestBody Tourist tourist) {
+		try {
+			String msg = service.updateTouristDetails(tourist);
+			return new ResponseEntity<String>(msg, HttpStatus.OK);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new ResponseEntity<String>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+	@DeleteMapping("/delete/{id}")
+	public ResponseEntity<String> removeTourist(@PathVariable("id") Integer id) {
+		try {
+			// use service
+			String msg = service.deleteTourist(id);
+			return new ResponseEntity<String>(msg, HttpStatus.OK);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new ResponseEntity<String>(e.getMessage(), HttpStatus.NOT_FOUND);
+		}
+	}
+
+	@PatchMapping("/budgetModify/{id}/{hike}")
+	public ResponseEntity<String> modifyTouristBudgetById(@PathVariable("id") Integer id,
+			@PathVariable("hike") Float hikePercent) {
+		try {
+			String msg=service.updateTouristBudgetById(id, hikePercent);
+			return new ResponseEntity<String>(msg,HttpStatus.OK);
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+			return new ResponseEntity<String>(e.getMessage(),HttpStatus.NOT_FOUND);
+		}
+
+	}
+
 }
