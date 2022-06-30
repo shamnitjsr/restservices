228593bd3991ca2d5353c1b5013619d7d48f65e8
shamnitjsr <46891976+shamnitjsr@users.noreply.github.com>
2022-06-30 23:07:29 +0530

    SwaggerAPI

---
+++ touristmanagementapiSwaggerAPI/src/main/java/com/nitjsr/exception/ErrorDetails.java
@@ -0,0 +1,19 @@
+package com.nitjsr.exception;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class ErrorDetails {
+	
+	private LocalDateTime time;
+	private String msg;
+	private String status;
+
+}
---
+++ touristmanagementapiSwaggerAPI/src/main/java/com/nitjsr/exception/TouristNotFoundException.java
@@ -0,0 +1,10 @@
+package com.nitjsr.exception;
+
+public class TouristNotFoundException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+	
+	public TouristNotFoundException(String msg) {
+		super(msg);
+	}
+}
